3ff3510336b7d580927464a598ab964238ecde8a
cordero <misacordero252@gmail.com>
2026-08-31 17:16:01 -0600

    Final Config SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import { unified } from '@astrojs/markdown-remark';
 import rehypeExternalLinks from 'rehype-external-links';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://pelicano-bueno.vercel.app',
+  integrations: [sitemap()],
   markdown: {
     processor: unified({
       rehypePlugins: [
